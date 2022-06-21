36adbb8673546b7e852eb327e03c98933fabb453
Tomas Malfettani <tomasmalfettani@gmail.com>
2022-06-20 21:38:40 -0300

    first commit

---
+++ main.js
@@ -0,0 +1,29 @@
+const resultado = document.getElementById("bdvN");
+const barraDeVida1 = document.getElementById("bdv").style.width // !!!!
+const container = document.querySelector(".container-fluid");
+
+let barraDeVida = 100;
+let barraDeVida2 = 341; // !!!!
+
+container.addEventListener("click", (e) => {
+    
+    if(e.target.classList.contains("btn-danger")) {
+        barraDeVida--;
+        barraDeVida2--;
+        restarVida();
+        restarBarraDeVida(); // !!!!
+    }
+    
+
+    e.stopPropagation();
+})
+
+function restarVida() {
+    resultado.textContent = barraDeVida;
+}
+
+function restarBarraDeVida() {
+    barraDeVida1.textContent = barraDeVida2; /// !!!!
+}
+
+
